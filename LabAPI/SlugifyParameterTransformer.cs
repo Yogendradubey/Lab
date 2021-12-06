2b822235a61370d0c1ef019e323fc57e6b0cc96b
Shrikrishna Chopade <erssc92@gmail.com>
2021-12-06 16:05:34 +0530

    Created New Branch

--- LabAPI/SlugifyParameterTransformer.cs
+++ LabAPI/SlugifyParameterTransformer.cs
@@ -8,6 +8,8 @@ namespace LabAPI
         public string TransformOutbound(object value)
         {
             // Slugify value
+
+
             return value == null ? null : Regex.Replace(value.ToString(), "([a-z])([A-Z])", "$1-$2").ToLower();
         }
     }
